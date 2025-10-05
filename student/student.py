cd489e137d4e50b7b79c544cbb428f0425e24d73
rahaf-af <rahaf77553@gmail.com>
2025-10-05 10:15:08 +0300

    CLASSWORK

--- student/student.py
+++ student/student.py
@@ -3,8 +3,32 @@ class Student:
         self.name= name
         self.grades = []
 
+    def add_grade(self, grade):
+        if grade >= 0 and grade <=100 :
+            self.grades.append(grade)
+        else :
+            raise ValueError("grade must be between 0 and 100 inclusive")
+    def average(self):
+        if not self.grades:
+            return 0
+        total = 0
+        for grade in self.grades:
+            total += grade
+        average = total / len (self.grades)
+        return(average)
+
+
 
 if __name__ == "__main__":
     student_name = input("enter your name please: ")
     new_student = Student(student_name)
     print(f"welcome {new_student.name}")
+    new_grade = input ("add grade to student")
+    try:
+        new_student.add_grade(int(new_grade))
+    except ValueError:
+        raise ValueError("grade must be between 0 and 100 inclusive")
+    
+
+
+
--- student/test/test_student.py
+++ student/test/test_student.py
@@ -2,7 +2,25 @@ import unittest
 from student.student import Student
 
 class Teststudent(unittest.TestCase):
+    def setUp(self):
+        self.student =Student("rahaf")
 
     def test_create_student_object(self):
-        new_student = Student("rahaf")
-        self.assertEqual(new_student.name,"rahaf")
+        #new_student = Student("rahaf")
+        self.assertEqual(self.student.name,"rahaf")
+    def test_add_valid_grade(self):
+        self.student.add_grade(50)
+        self.assertIn (50,self.student.grades)
+    def test_add_invalid_grade(self):
+        with self.student.add_grade(ValueError):
+            self.student.add_grade(101)
+            self.student.add_grade(-1)
+        with self.student.add_grade(TypeError):
+            self.student.add_grade("hello")
+    def test_average_no_grades(self):
+        self.assertEqual(self.student.average(),0)
+
+    def test_average_with_grades(self):
+        for grade in [100, 70 ,80]:
+            self.student.add_grade(grade)
+        self.assertEqual(self.student.average(),40)
